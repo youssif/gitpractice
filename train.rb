2ea5be867fe51f5f231d0c134efa2d7e96681086
Youssif <abdulyoussif@gmail.com>
2013-07-23 07:29:46 -0700

    commiting train rb

---
+++ train.rb
@@ -0,0 +1,12 @@
+class Train
+  # Instantiate a new Train object.
+  #
+  # model - The String model of the train.
+  # speed - The Integer speed of the train, in MPH.
+  #
+  # Returns a new Train.
+  def initialize(model, speed)
+    @model = model
+    @speed = speed
+  end
+end
